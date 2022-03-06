366c10fc7bd38a37aca900d9c0336f6a2d072bfb
Sudalaimani09 <msudalaimani92@gmail.com>
2022-03-06 22:48:38 +0530

    RED123

---
+++ Test.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class Test {
+
+	private void employeeName(String name) {
+		System.out.println("Employee name is : " + name);
+	}
+
+	public static void main(String[] args) {
+		Test a = new Test();
+		a.employeeName("SudalaiMani");
+	}
+}
